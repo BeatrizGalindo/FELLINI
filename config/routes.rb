113d83e0bf5f0bca93f4c1a282c683bdbb260002
Beatriz Galindo <b.galindo@me.com>
2021-06-09 15:01:19 +0100

    add watch movies to already watch

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :movies, only: [:index, :show]
+  resources :movie_selections, only: [:index, :update]
+  get '/movie_selections/watched', to: 'movie_selections#watched', as: 'watched_movie_selections'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
