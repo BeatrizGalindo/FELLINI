f4dca8bc2945668a708e2c0e0d7e02fdc4368dc3
Nick Dillon <nicholasdillon@hotmail.co.uk>
2021-06-14 14:21:16 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     resources :movie_selections, only: [:create]
     resources :recommendations, only: [:create]
   end
-  
+
   resources :friends, only: [:index, :destroy]
   resources :movie_selections, only: [:index, :update]
   get '/movie_selections/watched', to: 'movie_selections#watched', as: 'watched_movie_selections'
@@ -16,11 +16,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index] do
     resources :friends, only: [:create]
   end
-
-  resources :movie_selections, only: [:index, :update]
-  get '/movie_selections/watched', to: 'movie_selections#watched', as: 'watched_movie_selections'
-  get '/movie_selections/favourites', to: 'movie_selections#favourites', as: 'favourites_movie_selections'
-
   resources :recommendations, only: [:index, :destroy]
-
 end
