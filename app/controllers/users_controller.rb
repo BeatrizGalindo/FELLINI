3e9f2ae4aaed63576d056afe75f2a4bbcd9e7a98
Nick Dillon <nicholasdillon@hotmail.co.uk>
2021-06-15 15:12:04 +0100

    Profile page setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
-
+    @my_friends = current_user.friends
+    @users = User.all
   end
 
   def show
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
       @users = User.where.not(id: current_user.id)
     end
 
-    @recommended_movies = @user.movie_recommendations
-    @suggested_movies = @user.recommended_movies
+    @recommended_movies = @user.movie_recommendations.order(created_at: :desc).first(4)
+    @suggested_movies = @user.recommended_movies.order(created_at: :desc).first(4)
   end
 end
